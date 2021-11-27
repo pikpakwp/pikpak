219693454e3941b96bf3abf5217c9770e50880d7
pikpakwp <95108265+pikpakwp@users.noreply.github.com>
2021-11-27 19:17:53 +0800

    Update share.vue

--- src/views/share.vue
+++ src/views/share.vue
@@ -8,7 +8,7 @@
     </n-alert>
     <br />
     <n-alert title="请先设置默认上传目录" type="error" v-if="!uploadFolder || !uploadFolder.id">
-      请先在文件列表操作选择设置默认 <a href="https://www.tjsky.net/?p=220#i-4" target="_blank"> <n-icon style="vertical-align: middle;" size="20" color="#d03050"><zoom-question></zoom-question></n-icon> </a>
+      请先在文件列表操作选择设置默认 <a href="https://pikpakwp.github.io/pikpak/" target="_blank"> <n-icon style="vertical-align: middle;" size="20" color="#d03050"><zoom-question></zoom-question></n-icon> </a>
     </n-alert>
     
     <n-alert title="默认目录" type="success" v-else>
